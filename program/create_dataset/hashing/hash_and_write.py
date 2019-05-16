c817f95260c4fd472ebee47f6f0da73f6a617326
Emma <emsalinha@gmail.com>
2019-05-16 10:04:42 +0200

    allow multiple threads and 1fps hashing

--- program/create_dataset/hashing/hash_and_write.py
+++ program/create_dataset/hashing/hash_and_write.py
@@ -2,7 +2,7 @@ import h5py
 import numpy as np
 import time
 
-def hash_and_write(movie_name, frame_paths, hash_method, hash_params):
+def hash_and_write(movie_name, frame_paths, hash_method, hash_params, fps = 25):
     """
     writes hashes one by one to a (newly created) HDF5 file in the working directory
     :param movie_name: 'movie name'
@@ -15,6 +15,7 @@ def hash_and_write(movie_name, frame_paths, hash_method, hash_params):
     total_time = 0
 
     hash_len = hash_params['hash_size'] * hash_params['hash_size']
+    frame_paths = frame_paths[0::fps]
     n_frames = len(frame_paths)
     data_size = (n_frames, hash_len)
     phashes = np.zeros(data_size)
@@ -29,14 +30,20 @@ def hash_and_write(movie_name, frame_paths, hash_method, hash_params):
 
     hdf5_store = h5py.File('hashes_{}.hdf5'.format(movie_name), 'a')
 
-    phashes = hdf5_store.create_dataset(dataset_name, phashes.shape, compression='gzip')
+    try:
+        phashes = hdf5_store.create_dataset(dataset_name, phashes.shape, compression='gzip')
 
-    for i in range(0, n_frames):
-        frame_path = frame_paths[i]
-        start = time.time()
-        phash = hash_method(frame_path, hash_params)
-        end = time.time()
-        total_time += (end-start)
-        phashes[i] = phash
+        for i in range(0, n_frames):
+            frame_path = frame_paths[i]
+            start = time.time()
+            phash = hash_method(frame_path, hash_params)
+            end = time.time()
+            total_time += (end-start)
+            phashes[i] = phash
 
-    return total_time/n_frames
+        speed = total_time/n_frames
+
+    except:
+        speed = 0
+
+    return speed
