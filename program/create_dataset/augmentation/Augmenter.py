abeb04e8b9960a0100e5151dff63a0de5147ee46
Emma <emsalinha@gmail.com>
2019-07-04 12:26:34 +0200

    refactor

--- program/create_dataset/augmentation/Augmenter.py
+++ program/create_dataset/augmentation/Augmenter.py
@@ -16,97 +16,89 @@ from create_dataset.augmentation.select_aug_params.MSSIM.MSSIM import MultiScale
 
 class Augmenter:
 
-  def __init__(self, config):
-    self.config = config
-    self.params_dir = str()
-    self.import_modules()
-    self.sample_folder = '/home/emsala/Documenten/Studie/These/phashing/program/' \
-                         'create_dataset/augmentation/sample_frames/sampled_frames/'
-    self.aug_methods = [ 'add_hsv', 'subtract_hsv', 'add', 'gauss', 'compress', 'contrast', 'subtract', None]
-    self.img_paths = glob.glob('/home/emsala/Documenten/Studie/These/phashing/program/create_dataset/augmentation/sample_frames/sampled_frames/*')
-    self.params_dir = '~/movie-drive/results/'
-    self.hasher = Hasher
-    self.hash_params = {
-      'method': None,
-      'augmentation': True,
-      'hash_size': None,
-      'high_freq_factor': 8,
-      'vertical': 0,
-      'horizontal': 0
-    }
+    def __init__(self, img_paths=None, aug_methods=None):
+        self.params_dir = str()
+        self.import_modules()
+        self.aug_methods = self.get_aug_methods(aug_methods)
+        self.img_paths = self.get_img_paths(img_paths)
+        self.params_dir = '~/movie-drive/results/'
+        self.hasher = Hasher
+        self.hash_params = {
+          'method': None,
+          'augmentation': True,
+          'hash_size': None,
+          'high_freq_factor': 8,
+          'vertical': 0,
+          'horizontal': 0
+        }
+        self.main()
 
+    def get_img_paths(self, img_paths):
+       if img_paths == None:
+           img_paths  = glob.glob('/home/emsala/Documenten/Studie/These/phashing/program/'
+                             'create_dataset/augmentation/sample_frames/sampled_frames/*')
+        return img_paths
 
-  with open('filename.pickle', 'rb') as handle:
-    b = pickle.load(handle)
+    def get_aug_methods(self, aug_methods):
+        if aug_methods == None:
+            aug_methods = [ 'add_hsv', 'subtract_hsv', 'add', 'gauss', 'compress', 'contrast', 'subtract', None]
+          return aug_methods
 
-  def main(self, color=True):
+    def main(self, color=True):
 
-    for hash_method in self.hasher.hash_methods:
+        for hash_method in self.hasher.hash_methods:
 
-      for aug_method in self.aug_methods:
+          for aug_method in self.aug_methods:
 
-        print(hash_method.__name__, ' ', aug_method)
+            print(hash_method.__name__, ' ', aug_method)
 
-        hashes = {}
-        self.hash_params['method'] = hash_method.__name__
+            hashes = {}
 
-        if aug_method == None:
-          hashes = []
-          for image_path in sorted(self.img_paths):
-            phash = self.hasher.hash(image_path, self.hash_params)
-            hashes.append(phash)
+            self.hash_params['method'] = hash_method.__name__
 
-        else:
-          param_moments = read_params(aug_method)
-          for ssid, moments in param_moments.items():
-            mean_param = int(moments[0])
+            if aug_method == None:
+              hashes = []
+              for image_path in sorted(self.img_paths):
+                phash = self.hasher.hash(image_path, self.hash_params)
+                hashes.append(phash)
 
-            phashes_aug = []
-            for image_path in sorted(self.img_paths):
-              phash_aug = self.augment_and_hash(image_path, hash_method, aug_method, param=mean_param, color=color)
-              phashes_aug.append(phash_aug)
+            else:
+              param_moments = read_params(aug_method)
+              for ssid, moments in param_moments.items():
+                mean_param = int(moments[0])
 
-            hashes[ssid] = phashes_aug
+                phashes_aug = []
+                for image_path in sorted(self.img_paths):
+                  phash_aug = self.augment_and_hash(image_path, hash_method, aug_method, param=mean_param, color=color)
+                  phashes_aug.append(phash_aug)
 
-        with open('{}_12/{}_hashes.pickle'.format(hash_method.__name__, aug_method), 'wb') as handle:
-          pickle.dump(hashes, handle, protocol=pickle.HIGHEST_PROTOCOL)
+                hashes[ssid] = phashes_aug
 
-  def augment_and_hash(self, img_path, hash_method, aug_method, param, color=True):
-      """
-      :param img: PIL image
-      :param method: augmentation method (str)
-      :parameter method parameters
-      :return: hashes
-      """
+            with open('{}_12/{}_hashes.pickle'.format(hash_method.__name__, aug_method), 'wb') as handle:
+              pickle.dump(hashes, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
-      augmenter = return_augmenter(param, aug_method)
-      img_array = open_image(img_path, color = color, hash_method = hash_method.__name__)
-      aug_img_array = augmenter.augment_images(img_array)
-      aug_img_array = aug_img_array.squeeze(axis = 0)
-      if color:
+    def augment_and_hash(self, img_path, hash_method, aug_method, param, color=True):
+        """
+        :param img: PIL image
+        :param method: augmentation method (str)
+        :parameter method parameters
+        :return: hashes
+        """
+
+        augmenter = return_augmenter(param, aug_method)
+        img_array = open_image(img_path, color = color, hash_method = hash_method.__name__)
+        aug_img_array = augmenter.augment_images(img_array)
+        aug_img_array = aug_img_array.squeeze(axis = 0)
+        if color:
           aug_img_array = cv2.cvtColor(aug_img_array, cv2.COLOR_RGB2GRAY)
-      else:
+        else:
           aug_img_array = aug_img_array.squeeze(axis=3)
 
-      phash = hash_method(aug_img_array, load_frame=False)
-      return phash
+        phash = hash_method(aug_img_array, load_frame=False)
+        return phash
 
 
 
 if __name__ == '__main__':
 
-    hash_methods = [DCT_hash, AVG_hash]
-    #main(image_paths, hash_methods, aug_methods)
-    main(image_paths, [AVG_hash], ['gauss'])
-    main(image_paths, [DCT_hash], ['gauss'])
-
-    # main(image_paths, [AVG_hash], [None])
-    # main(image_paths, [DCT_hash], [None])
-
-def main(_):
-  if __name__ == "__main__":
-      parser = argparse.ArgumentParser()
-      parser.add_argument('--VM', type=bool, default=False, help='Running on VM or not')
-      config = parser.parse_args()
-      main(config)
-
+    augmennt = Augmenter()
